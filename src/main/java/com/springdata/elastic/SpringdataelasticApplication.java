ecea74f801bbc75463d8d7ff04edf9193d09b622
onesmus Kahuhu <blessedeffulgence@gmail.com>
2022-06-18 21:38:23 +0300

    First commit for spring data elastic

---
+++ src/main/java/com/springdata/elastic/SpringdataelasticApplication.java
@@ -0,0 +1,13 @@
+package com.springdata.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataelasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataelasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springdata/elastic/SpringdataelasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdata.elastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataelasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
